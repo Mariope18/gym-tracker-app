4335d352d8779a0b78473553a9f54f8d923c53bd
Mario Pella <pellamario18@gmail.com>
2026-05-13 23:34:56 +0200

    feat: implement workout detail screen and navigation from home

--- app/src/main/java/com/mariope18/gymtracker/HomeScreen.kt
+++ app/src/main/java/com/mariope18/gymtracker/HomeScreen.kt
@@ -1,5 +1,6 @@
 package com.mariope18.gymtracker
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -30,15 +31,17 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.google.firebase.Firebase
 import com.google.firebase.auth.auth
 import com.google.firebase.firestore.firestore
-import com.mariope18.gymtracker.ui.theme.GymTrackerTheme
 
 @Composable()
-fun HomeScreen(modifier: Modifier = Modifier, onLogoutSuccess: () -> Unit = {}) {
+fun HomeScreen(
+    modifier: Modifier = Modifier,
+    onLogoutSuccess: () -> Unit = {},
+    onWorkoutClick: (String, String) -> Unit = {_, _ ->}
+    ) {
 
     val auth = remember { Firebase.auth }
     val db = remember { Firebase.firestore }
@@ -116,6 +119,7 @@ fun HomeScreen(modifier: Modifier = Modifier, onLogoutSuccess: () -> Unit = {}) 
                         modifier = Modifier
                             .padding(8.dp)
                             .fillMaxWidth()
+                            .clickable { onWorkoutClick(workout.id, workout.name) }
                     ) {
                         Text(
                             text = workout.name,
@@ -192,11 +196,3 @@ fun HomeScreen(modifier: Modifier = Modifier, onLogoutSuccess: () -> Unit = {}) 
 }
 
 data class Workout(val id: String, val name: String)
-
-@Preview(showBackground = true)
-@Composable
-fun HomeScreenPreview() {
-    GymTrackerTheme {
-        HomeScreen(onLogoutSuccess = {})
-    }
-}
